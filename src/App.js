81af97e51b284d5790590418af04c42d4ba080e9
rajath1993 <anand.rajath@gmail.com>
2020-09-11 22:25:53 -0400

    created posts, passed props
    Created different posts, passed props, modified css - using flexbox brought items to the center.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Post from './Post';
+
 
 function App() {
+  const [posts,setPosts] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="http://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram"
+          />
+      </div>
+      <div className="app__post">
+        {/* posts */}
+        <Post userName="Rajath" caption="Hello there!" imageUrl="https://splashbase.s3.amazonaws.com/unsplash/regular/tumblr_mnh0n9pHJW1st5lhmo1_1280.jpg"/>
+        <Post userName="Shashanku" caption="Call me Shanks" imageUrl="https://splashbase.s3.amazonaws.com/gratisography/regular/10H.jpg"/>
+        <Post userName="Gowthama" caption="Call me Buddha" imageUrl="https://splashbase.s3.amazonaws.com/unsplash/regular/tumblr_mtawb6Bzzy1st5lhmo1_1280.jpg"/>
+      </div>
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Post.css';
+import Avatar from '@material-ui/core/Avatar';
+
+function Post({userName, caption, imageUrl}) {
+    return (
+        <div className="post">
+            <div className="post__header">
+                <Avatar
+                    className="post__avatar"
+                    alt="Rajath"
+                    src="../../public/profilepic.jpeg"
+                />
+                <h3>{userName}</h3>
+            </div>
+            {/* header -> image -> userName -> ... */}
+            
+            
+            {/* image */}
+            <img className="post__image" src={imageUrl}/>
+
+            {/* userName + caption */}
+            <h4 className="post__text"><strong>{userName}</strong> {caption}</h4>
+        </div>
+    )
+}
+
+export default Post
